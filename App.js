3fc135c487a8d915afc74f9d0f1e9618fd4c0d7a
stela <stela.capsa@gmail.com>
2025-06-21 04:38:51 +0800

    second push

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Stela</Text>
+      <Image 
+      source={require('./assets/stela.jpeg')}
+      style={{width:150, Hight:150,borderRadius: 10}}
+      />
+      <Text style={{fontSize:30, fontWeight: 'bold'}}>Stela Bella</Text>
+      <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Stela good job</Text>
       <StatusBar style="auto" />
     </View>
   );
