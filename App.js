943bcea34fe743057c79a4ff1430b539019240c5
stela <stela.capsa@gmail.com>
2025-06-24 01:49:55 +0800

    scrollView

--- App.js
+++ App.js
@@ -1,25 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import {
+	// other imports
+  ScrollView,
+} from 'react-native';
 
 export default function App() {
   return (
+    <ScrollView>
     <View style={styles.container}>
+      
+      <Image
+      source = {{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg'}}
+      style={{ width: '100%', aspectRatio: 16 / 9 }} 
+        />
       <Image 
       source={require('./assets/IMG_1822.jpg')}
-      style={{width:100, Hight:100, borderRadius: 75}}
+      style={{
+        width:150, 
+        height:150, 
+        borderRadius: 150, 
+        borderWidth:5, 
+        borderColor: "white", 
+        marginTop: -75
+      }}
       />
-      <Text style={{fontSize:30, fontWeight: 'bold'}}> stela Bella</Text>
-      <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Stela good job</Text>
+      <Text style={{fontSize:30, fontWeight: 'bold'}}> Stela Capsa</Text>
+      <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Web Developer</Text>
       <StatusBar style="auto" />
     </View>
-  );
-}
+    
+    <Text style={{ padding: 10, fontSize: 16, lineHeight: 20 }}>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam
+    aliquam sem et tortor consequat id porta nibh. Pellentesque nec
+    nam aliquam sem. Placerat duis ultricies lacus sed. Non curabitur
+    gravida arcu ac tortor dignissim convallis aenean. Amet nisl purus
+    in mollis nunc. Vel elit scelerisque mauris pellentesque pulvinar
+    pellentesque. Sagittis orci a scelerisque purus semper eget duis
+    at tellus. Sed libero enim sed faucibus turpis in eu mi bibendum.
+    Duis at consectetur lorem donec massa sapien faucibus et molestie.
+    At ultrices mi tempus imperdiet nulla malesuada pellentesque elit
+    eget. Purus sit amet volutpat consequat mauris nunc congue nisi
+    vitae. Urna condimentum mattis pellentesque id nibh tortor id.
+    Consequat id porta nibh venenatis. Lectus vestibulum mattis
+    ullamcorper velit sed ullamcorper. Mauris a diam maecenas sed enim
+    ut sem. Volutpat commodo sed egestas egestas fringilla phasellus.
+    Turpis egestas integer eget aliquet nibh praesent tristique magna
+    sit. Congue mauris rhoncus aenean vel elit scelerisque. Tellus
+    integer feugiat scelerisque varius morbi enim. Consectetur a erat
+    nam at. Bibendum arcu vitae elementum curabitur vitae nunc. Sit
+    amet consectetur adipiscing elit. Rhoncus mattis rhoncus urna
+    neque viverra justo. Malesuada pellentesque elit eget gravida.
+    Vitae nunc sed velit dignissim sodales ut eu sem integer.
+  </Text>
+  </ScrollView>
+    
+    )}
+
+    
+
+
+
+  
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "white",
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
